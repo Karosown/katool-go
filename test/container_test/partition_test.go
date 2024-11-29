1b5ac30c14c607405abf8ce93258eddb93109ce2
ZonglinWu <wuhukaros@gmail.com>
2024-11-29 16:21:46 +0800

    fixed: 项目重构 + 分片处理适配流操作 demo

--- test/container_test/partition_test.go
+++ test/container_test/partition_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/karosown/katool/container/lists"
 	"github.com/karosown/katool/container/stream"
+	"github.com/karosown/katool/convert"
 )
 
 func Test_Partition(t *testing.T) {
@@ -174,12 +175,322 @@ func Test_Partition(t *testing.T) {
 			Sex:   0,
 			Money: 23456789,
 			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
+		}, {
+			Name:  "张三",
+			Age:   18,
+			Sex:   0,
+			Money: 23456789,
+		},
+		{
+			Name:  "李四",
+			Age:   28,
+			Sex:   1,
+			Money: 23456789,
+			Id:    1,
+		},
+		{
+			Name:  "王五",
+			Age:   38,
+			Sex:   0,
+			Money: 23456789,
+			Id:    2,
 		},
 	}
-	sum := lists.Partition(userList, 3).Stream().Reduce(0, func(cntValue any, nxt []User) any {
-		return stream.ToStream(&nxt).Reduce(cntValue, func(sumValue any, nxt User) any {
-			return sumValue.(int) + nxt.Money
+	sum := lists.Partition(userList, 15).Stream().Map(func(i []User) any {
+		return stream.ToStream(&i).Map(func(user User) any {
+			properties, _ := convert.CopyProperties(user, &UserVo{})
+			return *properties
+		}).ToList()
+	}).Reduce("", func(cntValue any, nxt any) any {
+		anies := nxt.([]any)
+		return stream.ToStream(&anies).Reduce(cntValue, func(sumValue any, nxt any) any {
+			return sumValue.(string) + nxt.(UserVo).Name
 		})
 	})
-	println(sum.(int))
+	println(sum.(string))
 }
