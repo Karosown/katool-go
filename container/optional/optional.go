cbab95a82259490c98b81c236fc7b4b452428752
ZonglinWu <wuhukaros@gmail.com>
2024-12-24 18:50:08 +0800

    fixed：optional.go

--- container/optional/optional.go
+++ container/optional/optional.go
@@ -7,3 +7,29 @@ func IsTrue[T any](valid bool, isTrue T, isFalse T) T {
 		return isFalse
 	}
 }
+
+func IsTrueByFunc[T any](valid bool, isTrue func() T, isFalse func() T) T {
+	if valid {
+		return isTrue()
+	} else {
+		return isFalse()
+	}
+}
+
+func FuncIsTrueByFunc[T any](valid func() bool, isTrue, isFalse func() T) T {
+	if valid() {
+		return isTrue()
+	} else {
+		return isFalse()
+	}
+}
+
+func EmptyStringFunc() string {
+	return ""
+}
+
+func Identity[T any](obj T) func() T {
+	return func() T {
+		return obj
+	}
+}
