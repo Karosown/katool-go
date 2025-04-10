abdcf1dae38319e626b818cf751bb7fee0bcb20b
ZonglinWu <wuhukaros@gmail.com>
2025-04-10 14:20:48 +0800

    fixed：2.0.

--- util/date.go
+++ util/date.go
@@ -9,3 +9,31 @@ func BeginEndTimeComputed(run func()) int64 {
 	run()
 	return time.Now().UnixNano() - begin
 }
+
+type PeriodTimeStamp struct {
+	Start int64
+	End   int64
+}
+
+type PeriodTime struct {
+	Start time.Time
+	End   time.Time
+}
+
+// GetPeriods 获取一些时间段，时间段的范围在start - end 之间，时间段的长度为 duration
+func GetPeriods(start, end time.Time, duration time.Duration) []*PeriodTime {
+	rv := make([]*PeriodTime, 0)
+	temp := start
+	for {
+		if temp.After(end) || temp.Equal(end) {
+			break
+		}
+		period := &PeriodTime{
+			Start: temp,
+			End:   temp.Add(duration),
+		}
+		temp = period.End
+		rv = append(rv, period)
+	}
+	return rv
+}
