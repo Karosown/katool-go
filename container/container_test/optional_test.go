702f5b22d884e29ff13a130a7c5cb3c57f6fcefa
ZonglinWu <wuhukaros@gmail.com>
2025-06-09 11:43:36 +0800

    fixed: switch执行流.

---
+++ container/container_test/optional_test.go
@@ -0,0 +1,46 @@
+package container
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/karosown/katool-go/container/optional"
+)
+
+type Temp[T any] struct {
+	Status  int
+	Data    T
+	Message string
+}
+
+func TestOptCase(t *testing.T) {
+	o := &optional.OptSwitch[Temp[any]]{}
+	i := 5
+	submit, err := o.Case(i == 1, func() (*Temp[any], error) {
+		return &Temp[any]{0, nil, "1"}, nil
+	}, func() (*Temp[any], error) {
+		t.Log("case 1 否")
+		return nil, errors.New("")
+	}, func() (*Temp[any], error) {
+		t.Log("case 2 否")
+		return nil, nil
+	}).Case(i == 2, func() (*Temp[any], error) {
+		return &Temp[any]{0, nil, "2"}, nil
+	}).Case(i == 3, func() (*Temp[any], error) {
+		return &Temp[any]{0, nil, "3"}, nil
+	}).Case(i == 5, func() (*Temp[any], error) {
+		t.Log("success")
+		return &Temp[any]{0, nil, "5"}, nil
+	}).
+		Break().
+		Case(i >= 5, func() (*Temp[any], error) {
+			t.Log("success1")
+			return &Temp[any]{0, nil, ">=5"}, nil
+		}).Default(func(res *Temp[any], err error) (*Temp[any], error) {
+		return res, err
+	}).Submit()
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(submit)
+}
--- container/optional/case_chain.go
+++ container/optional/case_chain.go
@@ -1,39 +1,61 @@
 package optional
 
-type OptSwitch struct {
+import (
+	"errors"
+
+	"github.com/karosown/katool-go/container/cutil"
+)
+
+type OptSwitch[T any] struct {
 	isRun      bool
 	isBreak    bool
-	lastResult bool
+	lastStatus bool
+	lastResult *T
+	lastError  error
 }
 
-func (t *OptSwitch) Case(flag bool, run ...func()) *OptSwitch {
+func (t *OptSwitch[T]) Case(flag bool, run ...func() (*T, error)) *OptSwitch[T] {
 	if t.isBreak {
 		return t
 	}
-	if flag {
-		t.lastResult = true
-		run[0]()
-	} else {
-		t.lastResult = false
-		run[1]()
+	if !cutil.IsEmpty(run) {
+		if flag {
+			t.lastStatus = true
+			t.lastResult, t.lastError = run[0]()
+			t.isRun = true
+		} else {
+			t.lastStatus = false
+			if len(run) >= 2 {
+				for i := 1; i < len(run); i++ {
+					t.lastResult, t.lastError = run[i]()
+					if errors.Is(t.lastError, nil) {
+						return t
+					}
+				}
+			}
+		}
 	}
 	return t
 }
 
-func (t *OptSwitch) CaseFunc(flag func() bool, run ...func()) *OptSwitch {
+func (t *OptSwitch[T]) CaseFunc(flag func() bool, run ...func() (*T, error)) *OptSwitch[T] {
 	return t.Case(flag(), run...)
 }
-func (t *OptSwitch) Break() *OptSwitch {
-	if t.lastResult {
+func (t *OptSwitch[T]) Break() *OptSwitch[T] {
+	if t.lastStatus {
 		t.isBreak = true
 	}
 	return t
 }
-func (t *OptSwitch) Default(run ...func()) *OptSwitch {
+func (t *OptSwitch[T]) Default(run ...func(res *T, err error) (*T, error)) *OptSwitch[T] {
 	if !t.isRun {
 		for _, fn := range run {
-			fn()
+			t.lastResult, t.lastError = fn(t.lastResult, t.lastError)
 		}
 	}
 	return t
+}
+
+func (t *OptSwitch[T]) Submit() (*T, error) {
+	return t.lastResult, t.lastError
 }
