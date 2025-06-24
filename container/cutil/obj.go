f04de2309d743966ac1da9a8d73c928953e941bb
ZonglinWu <wuhukaros@gmail.com>
2025-06-24 10:47:30 +0800

    fixed: cutil.

--- container/cutil/obj.go
+++ container/cutil/obj.go
@@ -2,6 +2,8 @@ package cutil
 
 import (
 	"reflect"
+	"strconv"
+	"unicode"
 )
 
 func IsBlank[T comparable](obj T) bool {
@@ -28,4 +30,33 @@ func IsBlank[T comparable](obj T) bool {
 	default:
 		return obj == *new(T) // Check for zero value of the type
 	}
+}
+func IsNumeric(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	return err == nil
+}
+func IsAllDigit(s string) bool {
+	for _, r := range s {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+	}
+	return true
+}
+func IsNumericAdvanced(s string) bool {
+	_, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		// 检查是否是空字符串
+		if s == "" {
+			return false
+		}
+		// 检查是否包含除数字和'.'的其他字符
+		for _, r := range s {
+			if !unicode.IsDigit(r) && r != '.' {
+				return false
+			}
+		}
+		return true
+	}
+	return true
 }
